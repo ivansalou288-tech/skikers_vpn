da39c067bde002535a7707c4534660eed090e716
Ноутбук Ezhik <ezhik@gmail.com>
2026-07-01 19:18:08 +0300

    api

--- main.py
+++ main.py
@@ -874,7 +874,7 @@ async def subscription_callback(callback: types.CallbackQuery):
         
         # Создаем клавиатуру только если подписка активна
         if is_enabled:
-            subscription_url = f"https://{PUBLIC_DOMAIN}/sub/{sub_id}"
+            subscription_url = f"https://{PUBLIC_DOMAIN}:2096/sub/{sub_id}"
             subscription_keyboard = InlineKeyboardMarkup(
                 inline_keyboard=[
                     # [InlineKeyboardButton(text="Использовать", url=subscription_url, style="primary", icon_custom_emoji_id='5271604874419647061')],
